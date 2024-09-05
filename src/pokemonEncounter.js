941c886bd16828bea16b2dae2d106682da038240
Damian <hello@damianpetrov.com>
2024-09-05 20:40:00 +1000

    feat: add css for card styling

--- src/pokemonEncounter.js
+++ src/pokemonEncounter.js
@@ -1,38 +1,79 @@
 let pokemonRenderArea = document.getElementById("pokemonEncounterArea");
 
 function renderPokemonData(pokemonData) {
-    pokemonRenderArea.innerText = pokemonData.name;
+	if (!pokemonData.name) {
+        return;
+    }
+
+    let pokemonContainerDiv = document.createElement("div");
+    pokemonContainerDiv.classList += "pokemonCardEntry";
+
+    let pokemonImage = document.createElement("img");
+    pokemonImage.src = pokemonData.image;
+    pokemonContainerDiv.appendChild(pokemonImage);
+
+    let pokemonHeading = document.createElement("h1");
+    pokemonHeading.innerText = pokemonData.name;
+    pokemonContainerDiv.appendChild(pokemonHeading);
+
+
+    let pokemonTypesHeading = document.createElement("h3");
+	pokemonTypesHeading.innerText = "Types:";
+	pokemonContainerDiv.appendChild(pokemonTypesHeading);
+
+    let pokemonTypeList = document.createElement("ul");
+    pokemonData.types.forEach((type) => {
+        let typeListItem = document.createElement("li");
+        typeListItem.innerText = type.type.name;
+        pokemonTypeList.appendChild(typeListItem);
+    });
+    pokemonContainerDiv.appendChild(pokemonTypeList);
+
+    let pokemonAudioButton = document.createElement("button");
+    pokemonAudioButton.innerText = "Play Cry";
+    pokemonAudioButton.addEventListener("click", () => {
+        let audio = new Audio(pokemonData.sound);
+        audio.play();
+    });
+    pokemonContainerDiv.appendChild(pokemonAudioButton);
+
+    pokemonRenderArea.appendChild(pokemonContainerDiv);
 }
 
 async function getPokemon() {
 	console.log("Fetching Pokemon");
 
-    // Hardcoded for development, replaces "pikachu" with a random number
-    // random number is ID from 1 to 1025
-	let apiResponse = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
-    let apiData = await apiResponse.json();
+	// Generate a random number between 1 and 1024 (1025 being the total number of Pokemon in the API)
+	let suitableNumber = Math.floor(Math.random() * 1025) + 1;
 
-    // console.log(apiData);
+	// Fetch the Pokemon data from the API using the generated number to get a random Pokemon
+	let apiResponse = await fetch(
+		"https://pokeapi.co/api/v2/pokemon/" + suitableNumber
+	);
+
+	let apiData = await apiResponse.json();
+
+	// console.log(apiData);
 	// Name, Types, Image, Sound
 	// let pokemonName = apiData.name;
 	// return pokemonName;
 
-    return {
-        name: apiData.name,
-        types: apiData.types,
-        image: apiData.sprites.other.home.front_default,
-        sound: apiData.cries.latest
-    }
+	return {
+		name: apiData.name,
+		types: apiData.types,
+		image: apiData.sprites.other.home.front_default,
+		sound: apiData.cries.latest,
+	};
 }
 
 let encounterButton = document.getElementById("pokemonEncounterButton");
 
 encounterButton.addEventListener("click", async (event) => {
-	console.log("Some block of code in the event listener");
-	
-    let pokemonResult = await getPokemon();
-	
-    console.log(pokemonResult);
+	console.log("Encounter button clicked!");
 
-    renderPokemonData(pokemonResult);
+	let pokemonResult = await getPokemon();
+
+	console.log(pokemonResult);
+
+	renderPokemonData(pokemonResult);
 });
