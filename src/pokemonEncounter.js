e85e7dafccaa2ea1fc4f002cf96d6e821cadedd2
Damian <hello@damianpetrov.com>
2024-09-05 19:52:08 +1000

    feat: make request to poke api

---
+++ src/pokemonEncounter.js
@@ -0,0 +1,28 @@
+async function getPokemon() {
+	console.log("Fetching Pokemon");
+
+    // Hardcoded for development, replaces "pikachu" with a random number
+    // random number is ID from 1 to 1025
+	let apiResponse = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+    let apiData = await apiResponse.json();
+
+    // console.log(apiData);
+	// Name, Types, Image, Sound
+	// let pokemonName = apiData.name;
+	// return pokemonName;
+
+    return {
+        name: apiData.name,
+        types: apiData.types,
+        image: apiData.sprites.other.home.front_default,
+        sound: apiData.cries.latest
+    }
+}
+
+let encounterButton = document.getElementById("pokemonEncounterButton");
+
+encounterButton.addEventListener("click", async (event) => {
+	console.log("Some block of code in the event listener");
+	let pokemonResult = await getPokemon();
+	console.log(pokemonResult);
+});
